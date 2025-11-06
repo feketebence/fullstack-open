32f0aa53d19ecb7ea55c693d074bb1459939d63e
Fekete Bence <fea2clj2@bosch.com>
2025-11-06 11:31:07 +0200

    Finish exercise 1-7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,18 +5,23 @@ const Button = ({ onClick, text }) => {
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad;
+    const avg = (good - bad) / all;
     return (
         <div>
             <h1>Statistics</h1>
             good: {good} <br />
             neutral: {neutral} <br />
             bad: {bad} <br />
+            <br />
+            all: {all} <br />
+            average: {avg} <br />
+            positive: {good / all} %
         </div>
     );
 };
 
 const App = () => {
-    // save clicks of each button to its own state
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
