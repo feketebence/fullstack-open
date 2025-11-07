0245cf11fa58ebf61076640af75c37e05410cd65
Fekete Bence <fea2clj2@bosch.com>
2025-11-07 12:16:14 +0200

    Finish exercise 2-10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,64 @@
 import { useState } from 'react';
 
+const Entries = ({ persons, filterText }) => {
+    const personsToShow = persons.filter((person) =>
+        person.name.toUpperCase().includes(filterText.toUpperCase())
+    );
+
+    return (
+        <>
+            <h2>Numbers</h2>
+            <ol>
+                {personsToShow.map((person) => (
+                    <li key={person.name}>
+                        {person.name} {person.number}
+                    </li>
+                ))}
+            </ol>
+        </>
+    );
+};
+
+const NameFilter = ({ searchText, onChange }) => {
+    return (
+        <>
+            Filter names with:{' '}
+            <input type="text" value={searchText} onChange={onChange}></input>
+        </>
+    );
+};
+
+const PersonForm = ({
+    name,
+    onNameChange,
+    number,
+    onNumberChange,
+    onSubmit
+}) => {
+    return (
+        <div>
+            <h3>Add new person</h3>
+            <form onSubmit={onSubmit}>
+                <div>
+                    name:{' '}
+                    <input type="text" value={name} onChange={onNameChange} />
+                    <br />
+                    number:{' '}
+                    <input
+                        type="text"
+                        value={number}
+                        onChange={onNumberChange}
+                    ></input>
+                </div>
+
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
 const App = () => {
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -41,50 +100,21 @@ const App = () => {
         setNewNumber('');
     };
 
-    const personsToShow = persons.filter((person) =>
-        person.name.toUpperCase().includes(searchText.toUpperCase())
-    );
-
     return (
         <div>
             <h2>Phonebook</h2>
-            Filter names with:{' '}
-            <input
-                type="text"
-                value={searchText}
+            <NameFilter
+                searchText={searchText}
                 onChange={handleSearchTextChange}
-            ></input>
-            <br />
-            <br />
-            <form onSubmit={handleAddPerson}>
-                <div>
-                    name:{' '}
-                    <input
-                        type="text"
-                        value={newName}
-                        onChange={handleNewNameChange}
-                    />
-                    <br />
-                    number:{' '}
-                    <input
-                        type="text"
-                        value={newNumber}
-                        onChange={handleNewNumberChange}
-                    ></input>
-                </div>
-
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <ol>
-                {personsToShow.map((person) => (
-                    <li key={person.name}>
-                        {person.name} {person.number}
-                    </li>
-                ))}
-            </ol>
+            />
+            <PersonForm
+                name={newName}
+                onNameChange={handleNewNameChange}
+                number={newNumber}
+                onNumberChange={handleNewNumberChange}
+                onSubmit={handleAddPerson}
+            />
+            <Entries persons={persons} filterText={searchText} />
         </div>
     );
 };
