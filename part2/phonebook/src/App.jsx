31bf870bd843475143b0ef9e55145c8964ae22fa
Fekete Bence <fea2clj2@bosch.com>
2025-11-07 12:01:06 +0200

    Finish exercise 2-9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from 'react';
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '234-2341-2134' }
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
+    const [searchText, setSearchText] = useState('');
 
     const handleNewNameChange = (event) => setNewName(event.target.value);
     const handleNewNumberChange = (event) => setNewNumber(event.target.value);
+    const handleSearchTextChange = (event) => setSearchText(event.target.value);
 
     const handleAddPerson = (event) => {
         event.preventDefault();
 
+        const nextPersonId = Math.max(...persons.map((p) => p.id)) + 1;
+
         const newPerson = {
+            id: nextPersonId,
             name: newName,
             number: newNumber
         };
@@ -33,9 +41,21 @@ const App = () => {
         setNewNumber('');
     };
 
+    const personsToShow = persons.filter((person) =>
+        person.name.toUpperCase().includes(searchText.toUpperCase())
+    );
+
     return (
         <div>
             <h2>Phonebook</h2>
+            Filter names with:{' '}
+            <input
+                type="text"
+                value={searchText}
+                onChange={handleSearchTextChange}
+            ></input>
+            <br />
+            <br />
             <form onSubmit={handleAddPerson}>
                 <div>
                     name:{' '}
@@ -57,10 +77,9 @@ const App = () => {
                     <button type="submit">add</button>
                 </div>
             </form>
-
             <h2>Numbers</h2>
             <ol>
-                {persons.map((person) => (
+                {personsToShow.map((person) => (
                     <li key={person.name}>
                         {person.name} {person.number}
                     </li>
