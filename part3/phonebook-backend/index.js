f385072343121f664b1a70797e952fd2eddd5ae9
Fekete Bence <fea2clj2@bosch.com>
2025-11-12 11:58:04 +0200

    Finish exercise 3-2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -29,6 +29,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+    response.send(`
+        Phonebook has info for ${persons.length} ${
+        persons.length > 1 ? 'people' : 'person'
+    }.
+    <br />
+    ${new Date().toISOString()}
+        `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
