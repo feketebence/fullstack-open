6c40cd4a7619b30f7d9b7387d68d00d9c5b7bc15
feketebence <feketebencetyping@gmail.com>
2025-11-20 17:51:59 +0200

    Add validation and related error handling to notes app

--- part3/sandbox/backend/models/note.js
+++ part3/sandbox/backend/models/note.js
@@ -16,7 +16,12 @@ mongoose
     });
 
 const noteSchema = new mongoose.Schema({
-    content: String,
+    content: {
+        type: String,
+        minLength: 5,
+        maxLength: 200,
+        required: true
+    },
     important: Boolean
 });
 
