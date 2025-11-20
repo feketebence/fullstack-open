7b4ea4b602f2a0b04d97bce15e372a9a29aed430
feketebence <feketebencetyping@gmail.com>
2025-11-20 17:51:58 +0200

    Make port configurable

--- part3/sandbox/backend/index.js
+++ part3/sandbox/backend/index.js
@@ -131,7 +131,7 @@ const unknownEndpointMiddleware = (request, response) => {
 
 app.use(unknownEndpointMiddleware);
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
