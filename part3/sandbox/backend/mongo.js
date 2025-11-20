3d653f33c54efb917cf9522d55d215e26e904de3
feketebence <feketebencetyping@gmail.com>
2025-11-20 17:51:59 +0200

    Add linter to notes app and fix linter errors

--- part3/sandbox/backend/mongo.js
+++ part3/sandbox/backend/mongo.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument');
-    process.exit(1);
+    console.log('give password as argument')
+    process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const dbUser = 'feketebencetyping_db_user';
-const appName = 'noteApp';
+const dbUser = 'feketebencetyping_db_user'
+const appName = 'noteApp'
 // const url = `mongodb+srv://${dbUser}:${password}@fullstack-open.p2ncusy.mongodb.net/?appName=${appName}`;
-const url = `mongodb+srv://${dbUser}:${password}@fullstack-open.p2ncusy.mongodb.net/${appName}?retryWrites=true&w=majority&appName=fullstack-open`;
-console.log(`Using connection string: ${url}`);
+const url = `mongodb+srv://${dbUser}:${password}@fullstack-open.p2ncusy.mongodb.net/${appName}?retryWrites=true&w=majority&appName=fullstack-open`
+console.log(`Using connection string: ${url}`)
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url, { family: 4 });
+mongoose.connect(url, { family: 4 })
 
 const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean
-});
+})
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
 //     content: 'DMX Krew makes really good sounds!',
@@ -36,7 +36,7 @@ const Note = mongoose.model('Note', noteSchema);
 
 Note.find({}).then((result) => {
     result.forEach((note) => {
-        console.log(note);
-    });
-    mongoose.connection.close();
-});
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
