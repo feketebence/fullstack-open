419d50782fdaa6bb4799de35b1fc7b8adab8d7a7
feketebence <feketebencetyping@gmail.com>
2025-11-25 13:51:47 +0200

    Finish exercise 5-1 and 5-2

--- part5/blog-list/frontend/src/App.jsx
+++ part5/blog-list/frontend/src/App.jsx
@@ -1,24 +1,108 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    const [user, setUser] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs))
+    }, [])
+
+    useEffect(() => {
+        const loggedInUserJSON = window.localStorage.getItem(
+            'loggedBlogListAppUser'
+        )
+
+        if (loggedInUserJSON) {
+            const user = JSON.parse(loggedInUserJSON)
+            setUser(user)
+            blogService.setToken(user.token)
+        }
+    }, [])
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const user = await loginService.login({ username, password })
+
+            window.localStorage.setItem(
+                'loggedBlogListAppUser',
+                JSON.stringify(user)
+            )
+            blogService.setToken(user.token)
+            setUser(user)
+        } catch {
+            setErrorMessage('wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
+    const handleLogout = (event) => {
+        event.preventDefault()
+
+        window.localStorage.removeItem('loggedBlogListAppUser')
+        blogService.setToken(null)
+        setUser(null)
+    }
+
+    if (user === null) {
+        return (
+            <div className="container">
+                <h2>Login</h2>
+
+                {errorMessage && <p>Error: {errorMessage}</p>}
+
+                <form onSubmit={handleLogin}>
+                    <div>
+                        <label>
+                            username{' '}
+                            <input
+                                type="text"
+                                value={username}
+                                onChange={({ target }) =>
+                                    setUsername(target.value)
+                                }
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            password{' '}
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={({ target }) =>
+                                    setPassword(target.value)
+                                }
+                            />
+                        </label>
+                    </div>
+                    <button type="submit">login</button>
+                </form>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <h2>blogs</h2>
+            <p>{user.name} is logged in</p>
+            <button onClick={handleLogout}>log out</button>
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    )
 }
 
 export default App
