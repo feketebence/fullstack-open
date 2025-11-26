4b3884b33def74f7d9802efab5b8e70b6c8e9c1a
feketebence <feketebencetyping@gmail.com>
2025-11-26 17:06:52 +0200

    Finish exercise 5-7

--- part5/blog-list/frontend/src/components/Blog.jsx
+++ part5/blog-list/frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} - <i>{blog.author}</i>
-    </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+    const [expanded, setExpanded] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} - {blog.author}{' '}
+            <button onClick={() => setExpanded(!expanded)}>
+                {expanded ? 'hide' : 'expand'}
+            </button>
+            {expanded && (
+                <>
+                    <br />
+                    {blog.url} <br />
+                    likes {blog.likes} <button>like</button>
+                    <br />
+                    {blog.author} <br />
+                </>
+            )}
+        </div>
+    )
+}
 
 export default Blog
