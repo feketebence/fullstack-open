7cb78456f943746b7c341d72daed09d487bf76a7
feketebence <feketebencetyping@gmail.com>
2025-11-27 16:04:16 +0200

    Prepare notes backend for e2e testing

--- part5/sandbox/backend/utils/logger.js
+++ part5/sandbox/backend/utils/logger.js
@@ -1,13 +1,9 @@
 const info = (...params) => {
-    if (process.env.NODE_ENV !== 'test') {
-        console.log('[INFO]', ...params)
-    }
+    console.log('[INFO]', ...params)
 }
 
 const error = (...params) => {
-    if (process.env.NODE_ENV !== 'test') {
-        console.error('[ERROR]', ...params)
-    }
+    console.error('[ERROR]', ...params)
 }
 
 module.exports = {
