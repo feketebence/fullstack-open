4b70c972e35919975cb7f1527eea1052dafc9fa4
feketebence <feketebencetyping@gmail.com>
2025-12-27 21:13:13 +0200

    Finish exercise 6-20

--- part6/anecdotes-react-query/src/App.jsx
+++ part6/anecdotes-react-query/src/App.jsx
@@ -1,37 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+    const handleVote = (anecdote) => {
+        console.log('vote')
+    }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+    const result = useQuery({
+        queryKey: ['anecdotes'],
+        queryFn: getAnecdotes,
+        retry: 1
+    })
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
+    if (result.isLoading) {
+        return <div>Loading anecdotes...</div>
+    }
 
-      <Notification />
-      <AnecdoteForm />
+    if (result.isError) {
+        return <div>Anecdote app is not available due to server errors...</div>
+    }
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
+    const anecdotes = result.data
+
+    return (
+        <div>
+            <h3>Anecdote app</h3>
+
+            <Notification />
+            <AnecdoteForm />
+
+            {anecdotes.map((anecdote) => (
+                <div key={anecdote.id}>
+                    <div>{anecdote.content}</div>
+                    <div>
+                        has {anecdote.votes} votes{' '}
+                        <button onClick={() => handleVote(anecdote)}>
+                            vote
+                        </button>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default App
