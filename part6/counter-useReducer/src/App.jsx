2b70bceaccdb41d7bb2d791ea7ef5452f568db45
feketebence <feketebencetyping@gmail.com>
2025-12-28 18:07:35 +0200

    Progress in part 6d counter app: pass state to components via props

--- part6/counter-useReducer/src/App.jsx
+++ part6/counter-useReducer/src/App.jsx
@@ -1,31 +1,33 @@
 import { useReducer } from 'react'
+import Display from './components/Display'
+import Button from './components/Button'
 
 const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1
-    case 'DEC':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
+    switch (action.type) {
+        case 'INC':
+            return state + 1
+        case 'DEC':
+            return state - 1
+        case 'ZERO':
+            return 0
+        default:
+            return state
+    }
 }
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
+    const [counter, counterDispatch] = useReducer(counterReducer, 0)
 
-  return (
-    <div>
-      <div>{counter}</div>
-      <div>
-        <button onClick={() => counterDispatch({ type: 'INC' })}>+</button>
-        <button onClick={() => counterDispatch({ type: 'DEC' })}>-</button>
-        <button onClick={() => counterDispatch({ type: 'ZERO' })}>0</button>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <Display counter={counter} />
+            <div>
+                <Button dispatch={counterDispatch} type="INC" label="+" />
+                <Button dispatch={counterDispatch} type="DEC" label="-" />
+                <Button dispatch={counterDispatch} type="ZERO" label="0" />
+            </div>
+        </div>
+    )
 }
 
 export default App
