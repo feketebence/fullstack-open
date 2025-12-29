b5a29afb512c6306115ee8c6678af9f0b46db5ed
feketebence <feketebencetyping@gmail.com>
2025-12-29 17:04:25 +0200

    Progress in part 7a: add more components and parameterized routes to the notes app

--- part7/notes-app/src/App.jsx
+++ part7/notes-app/src/App.jsx
@@ -1,38 +1,90 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    Navigate
+} from 'react-router-dom'
+import { useState } from 'react'
 
 import Notes from './components/Notes'
 import Users from './components/Users'
 import Home from './components/Home'
+import Note from './components/Note'
+import Login from './components/Login'
 
 const App = () => {
     const padding = {
         padding: 5
     }
 
+    const [notes, setNotes] = useState([
+        {
+            id: 1,
+            content: 'HTML is easy',
+            important: true,
+            user: 'Matti Luukkainen'
+        },
+        {
+            id: 2,
+            content: 'Browser can execute only JavaScript',
+            important: false,
+            user: 'Matti Luukkainen'
+        },
+        {
+            id: 3,
+            content: 'Most important methods of HTTP-protocol are GET and POST',
+            important: true,
+            user: 'Arto Hellas'
+        }
+    ])
+
+    const [user, setUser] = useState(null)
+
+    const login = (user) => {
+        setUser(user)
+    }
+
     return (
-        <Router>
-            <div>
-                <Link style={padding} to="/">
-                    home
-                </Link>
-                <Link style={padding} to="/notes">
-                    notes
-                </Link>
-                <Link style={padding} to="/users">
-                    users
-                </Link>
-            </div>
+        <div>
+            <Router>
+                <div>
+                    <Link style={padding} to="/">
+                        home
+                    </Link>
+                    <Link style={padding} to="/notes">
+                        notes
+                    </Link>
+                    <Link style={padding} to="/users">
+                        users
+                    </Link>
+                    {user ? (
+                        <em>{user} logged in</em>
+                    ) : (
+                        <Link style={padding} to="/login">
+                            login
+                        </Link>
+                    )}
+                </div>
 
-            <Routes>
-                <Route path="/notes" element={<Notes />} />
-                <Route path="/users" element={<Users />} />
-                <Route path="/" element={<Home />} />
-            </Routes>
-
+                <Routes>
+                    <Route path="/notes/:id" element={<Note notes={notes} />} />
+                    <Route path="/notes" element={<Notes notes={notes} />} />
+                    <Route
+                        path="/users"
+                        element={
+                            user ? <Users /> : <Navigate replace to="/login" />
+                        }
+                    />
+                    <Route path="/login" element={<Login onLogin={login} />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
             <div>
-                <i>Note app, Department of Computer Science 2024</i>
+                <br />
+                <em>Note app, Department of Computer Science 2023</em>
             </div>
-        </Router>
+        </div>
     )
 }
 
--- part7/notes-app/src/components/Users.jsx
+++ part7/notes-app/src/components/Users.jsx
@@ -1,7 +1,11 @@
 const Users = () => (
     <div>
-        {' '}
-        <h2>Users</h2>{' '}
+        <h2>TKTL notes app</h2>
+        <ul>
+            <li>Matti Luukkainen</li>
+            <li>Juha Tauriainen</li>
+            <li>Arto Hellas</li>
+        </ul>
     </div>
 )
 
