b04db123869976ec0a1a258b26c083e7ad5401af
feketebence <feketebencetyping@gmail.com>
2026-01-06 01:58:44 +0200

    Finish exercise 7-2

--- part7/anecdotes-routed/src/App.jsx
+++ part7/anecdotes-routed/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useMatch } from 'react-router-dom'
 
 const Menu = () => {
     const padding = {
@@ -25,7 +25,11 @@ const AnecdoteList = ({ anecdotes }) => (
         <h2>Anecdotes</h2>
         <ul>
             {anecdotes.map((anecdote) => (
-                <li key={anecdote.id}>{anecdote.content}</li>
+                <li key={anecdote.id}>
+                    <Link to={`/anecdotes/${anecdote.id}`}>
+                        {anecdote.content}
+                    </Link>
+                </li>
             ))}
         </ul>
     </div>
@@ -115,6 +119,20 @@ const CreateNew = ({ onCreate }) => {
     )
 }
 
+const Anecdote = ({ anecdote }) => {
+    return (
+        <>
+            <h2>
+                {anecdote.content} by {anecdote.author}
+            </h2>
+            <div>has {anecdote.votes} votes</div>
+            <div>
+                for more info see <a href={anecdote.info}>{anecdote.info}</a>
+            </div>
+        </>
+    )
+}
+
 const App = () => {
     const [anecdotes, setAnecdotes] = useState([
         {
@@ -153,6 +171,11 @@ const App = () => {
         setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)))
     }
 
+    const match = useMatch('/anecdotes/:id')
+    const anecdote = match
+        ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
+        : null
+
     return (
         <>
             <div>
@@ -164,6 +187,10 @@ const App = () => {
                 <Route
                     path="/anecdotes"
                     element={<AnecdoteList anecdotes={anecdotes} />}
+                />
+                <Route
+                    path="/anecdotes/:id"
+                    element={<Anecdote anecdote={anecdote} />}
                 />
                 <Route
                     path="/create"
