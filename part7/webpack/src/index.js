8c55e16bde83289fb1958d4ab928344e1028a1ec
feketebence <feketebencetyping@gmail.com>
2026-01-12 17:30:05 +0200

    Progress in part 7d: add React and a simple Component

--- part7/webpack/src/index.js
+++ part7/webpack/src/index.js
@@ -1,7 +1,8 @@
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+
+import React from 'react' // we need this now also in the entrypoint
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const hello = (name) => {
-    console.log(`hello ${name}`)
-}
-
-App()
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
