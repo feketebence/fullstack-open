e9a612130d8ce55fd12c131c6d611c2e33a55254
feketebence <feketebencetyping@gmail.com>
2026-02-17 12:51:28 +0200

    Progress in part 9b: setup typescript project

---
+++ part9/ts-intro/multiplier.ts
@@ -0,0 +1,7 @@
+type Operation = 'multiply' | 'add' | 'divide'
+
+const multiplicator = (a: number, b: number, printText: string) => {
+    console.log(printText, a * b)
+}
+
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:')
