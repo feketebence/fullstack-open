ea6f30aef1e2ef93ac3c548b3035961d3e9311cf
feketebence <feketebencetyping@gmail.com>
2026-02-24 17:34:07 +0200

    Progress in part 10: use env variables

--- part10/rate-repository-app/mobile/App.js
+++ part10/rate-repository-app/mobile/App.js
@@ -1,10 +1,13 @@
 import { NativeRouter } from 'react-router-native'
+import Constants from 'expo-constants'
 
 import Main from './src/components/Main'
 import createApolloClient from './src/utils/apolloClient'
 import { ApolloProvider } from '@apollo/client'
 
 const apolloClient = createApolloClient()
+
+console.log(`Running the app in ${Constants.expoConfig.extra.env} mode.`)
 
 const App = () => {
     return (
